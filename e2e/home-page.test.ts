db00566733d966ee13549ae036937ddeaac01953
Francisco <github.backstage538@passmail.net>
2025-03-28 21:24:39 +0100

    Add tests e2e

---
+++ e2e/home-page.test.ts
@@ -0,0 +1,8 @@
+import { test, expect } from '@playwright/test';
+
+test('Show heading', async ({ page }) => {
+  await page.goto('/');
+
+  await expect(page).toHaveTitle(/Zyrcode/);
+  await expect(page.getByRole('heading', { name: 'Zyrcode' })).toBeVisible();
+});
