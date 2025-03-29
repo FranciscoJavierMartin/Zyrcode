21bbcf7a068cea818cb57046da0c26ea9784f464
Francisco <github.backstage538@passmail.net>
2025-03-29 14:01:40 +0100

    Split routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import { ROUTES } from '@/router/routes';
 import HomePage from '@/pages/HomePage/HomePage.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
+      ...ROUTES.HOME,
       component: HomePage,
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      redirect: '/',
     },
   ],
 });
