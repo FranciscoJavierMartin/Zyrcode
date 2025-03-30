907e8831ea16b083bf465618a08c01728486a142
Francisco <github.backstage538@passmail.net>
2025-03-30 23:57:11 +0200

    Update test

--- src/modules/home/pages/home-page.test.ts
+++ src/modules/home/pages/home-page.test.ts
@@ -2,17 +2,21 @@ import { describe, test, expect } from 'vitest';
 import { mount } from '@vue/test-utils';
 import HomePage from '@/modules/home/pages/home-page.vue';
 import i18n from '@/i18n';
+import router from '@/router';
 
 describe('<HomePage />', () => {
-  test('renders heading', () => {
+  test('renders header', () => {
     const wrapper = mount(HomePage, {
       global: {
-        plugins: [i18n],
+        plugins: [i18n, router],
       },
     });
     expect(wrapper.find('h1').text()).toBe('Zyrcode');
     expect(wrapper.find('.quote-typing').text()).contains(
       'Where Code Meets Creativity.',
     );
+    const createNotebookButton = wrapper.find('a');
+    expect(createNotebookButton.attributes('href')).toBe('#');
+    expect(createNotebookButton.text()).toBe('Create notebook');
   });
 });
