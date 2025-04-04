e746a95beda720d33e91c925fc5a2517364ced74
Francisco <github.backstage538@passmail.net>
2025-04-04 19:00:03 +0200

    Refactor debounce

--- src/modules/common/helpers/debounce.ts
+++ src/modules/common/helpers/debounce.ts
@@ -1,16 +1,14 @@
 // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
-export function debounce(fn: Function, wait: number) {
+export const debounce = (fn: Function, wait: number) => {
   let timer: NodeJS.Timeout;
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  return function (...args: any) {
+  return (...args: unknown[]) => {
     if (timer) {
       clearTimeout(timer);
     }
-    // eslint-disable-next-line @typescript-eslint/no-this-alias
-    const context = this;
+
     timer = setTimeout(() => {
-      fn.apply(context, args);
+      fn.apply(this, args);
     }, wait);
   };
-}
+};
