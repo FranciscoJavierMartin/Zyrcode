4b4fdcbcca39c3b296315f0ec34c133aca7e01db
Francisco <github.backstage538@passmail.net>
2025-04-11 19:36:22 +0200

    Change icons

--- src/modules/common/components/toggle-theme/toggle-theme.vue
+++ src/modules/common/components/toggle-theme/toggle-theme.vue
@@ -4,13 +4,11 @@
     @click="toggleTheme"
     class="bg-background cursor-pointer"
   >
-    <Icon
-      icon="radix-icons:moon"
-      class="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90"
+    <Moon
+      class="size-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90"
     />
-    <Icon
-      icon="radix-icons:sun"
-      class="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0"
+    <Sun
+      class="size-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0"
     />
     <span class="sr-only">{{ $t('a11y.toggleTheme') }}</span>
   </Button>
@@ -18,8 +16,8 @@
 
 <script setup lang="ts">
 import { Button } from '@/modules/common/components/ui/button';
-import { Icon } from '@iconify/vue';
 import { useColorMode } from '@vueuse/core';
+import { Sun, Moon } from 'lucide-vue-next';
 
 const mode = useColorMode({ disableTransition: false });
 
