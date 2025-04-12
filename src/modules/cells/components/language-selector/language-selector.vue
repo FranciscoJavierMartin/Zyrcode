1f38930e41a1bb81d84f5077fb42912d2903fbd2
Francisco <github.backstage538@passmail.net>
2025-04-12 14:35:11 +0200

    Change language

---
+++ src/modules/cells/components/language-selector/language-selector.vue
@@ -0,0 +1,35 @@
+<template>
+  <DropdownMenu>
+    <DropdownMenuTrigger as-child>
+      <Button variant="hover" class="h-8 cursor-pointer rounded shadow-sm">
+        {{ language }}
+      </Button>
+    </DropdownMenuTrigger>
+    <DropdownMenuContent class="w-56" align="end">
+      <DropdownMenuRadioGroup v-model="language">
+        <DropdownMenuRadioItem value="typescript">
+          TypeScript
+        </DropdownMenuRadioItem>
+        <DropdownMenuRadioItem value="javascript">
+          JavaScript
+        </DropdownMenuRadioItem>
+      </DropdownMenuRadioGroup>
+    </DropdownMenuContent>
+  </DropdownMenu>
+</template>
+
+<script lang="ts" setup>
+import { Button } from '@/modules/common/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuTrigger,
+} from '@/modules/common/components/ui/dropdown-menu';
+
+const language = defineModel({
+  type: String,
+  required: true,
+});
+</script>
--- src/modules/code-editor/components/code-editor/code-editor.vue
+++ src/modules/code-editor/components/code-editor/code-editor.vue
@@ -20,12 +20,12 @@ import parserTypeScript from 'prettier/plugins/typescript';
 import { useColorMode } from '@vueuse/core';
 import { getSuggestion } from '@/modules/code-editor/helpers/get-suggestion';
 import { CompletionFormatter } from '@/modules/code-editor/helpers/completion-formatter';
-import LanguageSelector from '@/modules/code-editor/components/language-selector/language-selector.vue';
+import type { Language } from '@/modules/cells/interfaces/languages';
 import '@/modules/code-editor/utils/worker';
 
 const editorRef = useTemplateRef<HTMLDivElement>('editor');
 const editorId = ref('');
-const props = defineProps<{ language: string }>();
+const props = defineProps<{ language: Language }>();
 const code = defineModel({ required: true, type: String });
 const inlineCompletionsProvider = ref<monaco.IDisposable | undefined>();
 const theme = useColorMode({ disableTransition: false });
@@ -106,6 +106,7 @@ watch(
   (newLanguage: string) => {
     inlineCompletionsProvider.value =
       registerInlineCompletionsProvider(newLanguage);
+    editor.value?.setModel(monaco.editor.createModel(code.value, newLanguage));
 
     onWatcherCleanup(() => {
       inlineCompletionsProvider.value?.dispose();
