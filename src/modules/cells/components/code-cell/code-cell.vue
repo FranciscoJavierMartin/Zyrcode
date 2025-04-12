c834429b73e89f2f416842cd629e98af50d49af9
Francisco <github.backstage538@passmail.net>
2025-04-12 11:44:38 +0200

    Style buttons

--- src/modules/cells/components/code-cell/code-cell.vue
+++ src/modules/cells/components/code-cell/code-cell.vue
@@ -1,7 +1,19 @@
 <template>
   <div class="flex flex-col">
-    <div>
-      <Button @click="format">format</Button>
+    <div class="mb-2 flex px-2">
+      <div class="flex flex-1 gap-2">
+        <Button class="cursor-pointer rounded p-1 shadow-sm">
+          <ArrowUp class="size-[1.2rem]" />
+        </Button>
+        <Button class="cursor-pointer rounded p-1 shadow-sm">
+          <ArrowDown class="size-[1.2rem]" />
+        </Button>
+      </div>
+      <div class="flex">
+        <Button @click="format" class="cursor-pointer rounded p-1 shadow-sm">
+          <PencilRuler class="size-[1.2rem]" />
+        </Button>
+      </div>
     </div>
     <ResizablePanelGroup direction="horizontal" class="min-h-[300px] w-full">
       <ResizablePanel :default-size="50">
@@ -24,10 +36,13 @@ import CodePreview from '@/modules/cells/components/code-preview/code-preview.vu
 import ResizableHandle from '@/modules/common/components/ui/resizable/ResizableHandle.vue';
 import ResizablePanel from '@/modules/common/components/ui/resizable/ResizablePanel.vue';
 import ResizablePanelGroup from '@/modules/common/components/ui/resizable/ResizablePanelGroup.vue';
+import { ArrowDown, ArrowUp, PencilRuler } from 'lucide-vue-next';
+import { useMediaQuery } from '@vueuse/core';
 
 const code = ref<string>(`const a = 12356;\nconsole.log(a);`);
 const transpiledCode = ref<string>('');
 const editor = useTemplateRef('editor');
+const isLargeScreen = useMediaQuery('(min-width: 1024px)');
 
 function format(): void {
   editor.value?.formatCode();
