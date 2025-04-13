26fb65222b7aa5c28ff5b44d92aaf6ba8ba123a1
Francisco <github.backstage538@passmail.net>
2025-04-13 13:51:40 +0200

    Display multiple cells

--- src/modules/notebook/pages/notebook-page.vue
+++ src/modules/notebook/pages/notebook-page.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="mt-13">
-    <CodeCell />
+    <CodeCell v-for="cell of cells" :key="cell.id" />
   </div>
 </template>
 
 <script setup lang="ts">
 import CodeCell from '@/modules/cells/components/code-cell/code-cell.vue';
+import { useCellsStore } from '@/modules/cells/store/cells';
 import useBundler from '@/modules/notebook/composables/use-bundler';
+import { storeToRefs } from 'pinia';
 useBundler();
+const { cells } = storeToRefs(useCellsStore());
 </script>
