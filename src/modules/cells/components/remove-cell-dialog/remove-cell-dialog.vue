e3ed61713cf63333cb3b5407c5a00c6560a33f60
Francisco <github.backstage538@passmail.net>
2025-04-13 20:41:38 +0200

    Style button

---
+++ src/modules/cells/components/remove-cell-dialog/remove-cell-dialog.vue
@@ -0,0 +1,41 @@
+<template>
+  <AlertDialog>
+    <AlertDialogTrigger as-child>
+      <Button
+        variant="hover"
+        class="button-icon dark:text-foreground dark:hover:text-background text-red-500"
+      >
+        <Trash2 class="size-5" />
+      </Button>
+    </AlertDialogTrigger>
+    <AlertDialogContent>
+      <AlertDialogHeader>
+        <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+        <AlertDialogDescription>
+          This action cannot be undone. This will permanently delete your
+          account and remove your data from our servers.
+        </AlertDialogDescription>
+      </AlertDialogHeader>
+      <AlertDialogFooter>
+        <AlertDialogCancel>Cancel</AlertDialogCancel>
+        <AlertDialogAction>Continue</AlertDialogAction>
+      </AlertDialogFooter>
+    </AlertDialogContent>
+  </AlertDialog>
+</template>
+
+<script setup lang="ts">
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/modules//common/components/ui/alert-dialog';
+import { Button } from '@/modules/common/components/ui/button';
+import { Trash2 } from 'lucide-vue-next';
+</script>
