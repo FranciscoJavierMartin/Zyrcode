bfadcadc6c2e117d6ddafad400b30fa38263f908
Francisco <github.backstage538@passmail.net>
2025-04-16 19:59:12 +0200

    Apply styles to svg

--- src/modules/common/components/ui/icon-text-button/icon-text-button.vue
+++ src/modules/common/components/ui/icon-text-button/icon-text-button.vue
@@ -1,6 +1,6 @@
 <template>
   <button class="icon-text-button">
-    <Terminal class="absolute top-1.5 left-1.5 size-5" />
+    <Terminal class="" />
     <span> {{ text }} </span>
   </button>
 </template>
@@ -17,7 +17,11 @@ defineProps<{ text: string }>();
 .icon-text-button {
   @apply bg-background text-foreground hover:bg-foreground hover:text-background relative flex h-8 w-8 max-w-8 cursor-pointer items-center overflow-x-hidden rounded-md px-2 py-3 text-sm font-medium shadow-xs transition-[max-width] duration-500 hover:max-w-80 disabled:pointer-events-none disabled:opacity-50 lg:w-auto;
 
-  span {
+  > svg {
+    @apply absolute top-1.5 left-1.5 size-5;
+  }
+
+  > span {
     @apply ml-6 hidden whitespace-nowrap lg:block;
   }
 }
