7d0ef2de51ef6b438566109ddc2e3b82439130b0
Francisco <github.backstage538@passmail.net>
2025-04-16 19:12:35 +0200

    keep width on mobile

--- src/modules/common/components/ui/icon-text-button/icon-text-button.vue
+++ src/modules/common/components/ui/icon-text-button/icon-text-button.vue
@@ -15,10 +15,10 @@ defineProps<{ text: string }>();
 @reference '@/assets/styles.css';
 
 .icon-text-button {
-  @apply bg-background text-foreground hover:bg-foreground hover:text-background relative flex h-8 max-w-8 cursor-pointer items-center overflow-x-hidden rounded-md px-2 py-3 text-sm font-medium shadow-xs transition-[max-width] duration-500 hover:max-w-80 disabled:pointer-events-none disabled:opacity-50;
+  @apply bg-background text-foreground hover:bg-foreground hover:text-background relative flex h-8 w-8 max-w-8 cursor-pointer items-center overflow-x-hidden rounded-md px-2 py-3 text-sm font-medium shadow-xs transition-[max-width] duration-500 hover:max-w-80 disabled:pointer-events-none disabled:opacity-50 lg:w-auto;
 
   span {
-    @apply ml-6 whitespace-nowrap;
+    @apply ml-6 hidden whitespace-nowrap lg:block;
   }
 }
 </style>
