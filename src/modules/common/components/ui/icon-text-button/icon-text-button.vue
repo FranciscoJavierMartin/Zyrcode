33253afe8cd215bfe40acb829088a4e2397e3a1b
Francisco <github.backstage538@passmail.net>
2025-04-16 19:05:51 +0200

    Add classes

--- src/modules/common/components/ui/icon-text-button/icon-text-button.vue
+++ src/modules/common/components/ui/icon-text-button/icon-text-button.vue
@@ -1,7 +1,5 @@
 <template>
-  <button
-    class="bg-foreground text-background relative flex h-8 max-w-8 items-center overflow-x-hidden px-2 py-3 transition-[max-width] duration-500 hover:max-w-80"
-  >
+  <button class="icon-text-button">
     <Terminal class="absolute top-1.5 left-1.5 size-5" />
     <span class="ml-6 whitespace-nowrap"> {{ text }} </span>
   </button>
@@ -12,3 +10,11 @@ import { Terminal } from 'lucide-vue-next';
 
 defineProps<{ text: string }>();
 </script>
+
+<style>
+@reference '@/assets/styles.css';
+
+.icon-text-button {
+  @apply bg-background text-foreground hover:bg-foreground hover:text-background relative flex h-8 max-w-8 items-center overflow-x-hidden rounded-md px-2 py-3 text-sm font-medium shadow-xs transition-[max-width] duration-500 hover:max-w-80 disabled:pointer-events-none disabled:opacity-50;
+}
+</style>
