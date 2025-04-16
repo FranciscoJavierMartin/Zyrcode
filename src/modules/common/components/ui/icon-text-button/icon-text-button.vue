77e520bbf515ca98be29a5ece2a69c5289dab09a
Francisco <github.backstage538@passmail.net>
2025-04-16 19:09:26 +0200

    Style text

--- src/modules/common/components/ui/icon-text-button/icon-text-button.vue
+++ src/modules/common/components/ui/icon-text-button/icon-text-button.vue
@@ -1,7 +1,7 @@
 <template>
   <button class="icon-text-button">
     <Terminal class="absolute top-1.5 left-1.5 size-5" />
-    <span class="ml-6 whitespace-nowrap"> {{ text }} </span>
+    <span> {{ text }} </span>
   </button>
 </template>
 
@@ -15,6 +15,10 @@ defineProps<{ text: string }>();
 @reference '@/assets/styles.css';
 
 .icon-text-button {
-  @apply bg-background text-foreground hover:bg-foreground hover:text-background relative flex h-8 max-w-8 items-center overflow-x-hidden rounded-md px-2 py-3 text-sm font-medium shadow-xs transition-[max-width] duration-500 hover:max-w-80 disabled:pointer-events-none disabled:opacity-50;
+  @apply bg-background text-foreground hover:bg-foreground hover:text-background relative flex h-8 max-w-8 cursor-pointer items-center overflow-x-hidden rounded-md px-2 py-3 text-sm font-medium shadow-xs transition-[max-width] duration-500 hover:max-w-80 disabled:pointer-events-none disabled:opacity-50;
+
+  span {
+    @apply ml-6 whitespace-nowrap;
+  }
 }
 </style>
