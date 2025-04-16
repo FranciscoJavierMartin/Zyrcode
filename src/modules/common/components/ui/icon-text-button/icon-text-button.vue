27ee9cde9002ae853733e4464ba3e03b90f9a6dd
Francisco <github.backstage538@passmail.net>
2025-04-16 20:11:41 +0200

    Place icon

--- src/modules/common/components/ui/icon-text-button/icon-text-button.vue
+++ src/modules/common/components/ui/icon-text-button/icon-text-button.vue
@@ -1,23 +1,23 @@
 <template>
   <button class="icon-text-button">
-    <Terminal class="" />
+    <div class="icon">
+      <slot />
+    </div>
     <span> {{ text }} </span>
   </button>
 </template>
 
 <script lang="ts" setup>
-import { Terminal } from 'lucide-vue-next';
-
 defineProps<{ text: string }>();
 </script>
 
-<style>
+<style scoped>
 @reference '@/assets/styles.css';
 
 .icon-text-button {
   @apply bg-background text-foreground hover:bg-foreground hover:text-background relative flex h-8 w-8 max-w-8 cursor-pointer items-center overflow-x-hidden rounded-md px-2 py-3 text-sm font-medium shadow-xs transition-[max-width] duration-500 hover:max-w-80 disabled:pointer-events-none disabled:opacity-50 lg:w-auto;
 
-  > svg {
+  .icon {
     @apply absolute top-1.5 left-1.5 size-5;
   }
 
