910d139e197b33ac31ed5e01e719afc77c3fc3d8
Francisco <github.backstage538@passmail.net>
2025-04-17 12:13:40 +0200

    Move to component

--- src/modules/notebook/pages/notebook-page.vue
+++ src/modules/notebook/pages/notebook-page.vue
@@ -9,28 +9,7 @@
         :language="cell.language"
       />
     </template>
-    <template v-else>
-      <div class="mx-auto flex max-w-6xl flex-col gap-10">
-        <div v-for="i of [1, 2, 3]" :key="i">
-          <div
-            class="grid-layout grid grid-cols-1 grid-rows-[40px_100px_100px_40px] gap-2.5 lg:grid-cols-2 lg:grid-rows-[40px_300px_40px]"
-          >
-            <Skeleton
-              class="grid-area-[toolbar] grid-area-toolbar h-full max-h-[300px] rounded-xl"
-            />
-            <Skeleton
-              class="grid-area-[toolbar] grid-area-editor h-full max-h-[300px] rounded-xl"
-            />
-            <Skeleton
-              class="grid-area-[toolbar] grid-area-preview h-full max-h-[300px] rounded-xl"
-            />
-            <Skeleton
-              class="grid-area-[toolbar] grid-area-console h-full max-h-[300px] rounded-xl"
-            />
-          </div>
-        </div>
-      </div>
-    </template>
+    <NotebookSkeleton v-else />
   </div>
 </template>
 
@@ -39,34 +18,8 @@ import CodeCell from '@/modules/cells/components/code-cell/code-cell.vue';
 import { useCellsStore } from '@/modules/cells/store/cells';
 import useBundler from '@/modules/notebook/composables/use-bundler';
 import { storeToRefs } from 'pinia';
-import Skeleton from '@/modules/common/components/ui/skeleton/Skeleton.vue';
+import NotebookSkeleton from '@/modules/notebook/components/notebook-skeleton/notebook-skeleton.vue';
 
 const { isServiceLoading } = useBundler();
 const { cells } = storeToRefs(useCellsStore());
 </script>
-
-<style>
-.grid-layout {
-  grid-template-areas: 'toolbar' 'editor' 'preview' 'console';
-
-  @media (width >= 64rem /* 1024px */) {
-    grid-template-areas: 'toolbar toolbar' 'editor preview' 'console console';
-  }
-}
-
-.grid-area-toolbar {
-  grid-area: toolbar;
-}
-
-.grid-area-editor {
-  grid-area: editor;
-}
-
-.grid-area-preview {
-  grid-area: preview;
-}
-
-.grid-area-console {
-  grid-area: console;
-}
-</style>
