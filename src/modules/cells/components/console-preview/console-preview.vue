8b00619395e56d1c99d7b4dd081ff8161f911c67
Francisco <github.backstage538@passmail.net>
2025-04-17 13:49:08 +0200

    Get log level

--- src/modules/cells/components/console-preview/console-preview.vue
+++ src/modules/cells/components/console-preview/console-preview.vue
@@ -1,12 +1,19 @@
 <template>
   <ol
-    class="bg-ring h-auto max-h-[300px] overflow-auto p-2 empty:p-0"
+    class="h-auto max-h-[300px] overflow-auto border border-red-500"
     style="interpolate-size: allow-keywords"
   >
     <li
       v-for="output of outputs"
       :key="output.id"
-      class="border-muted-foreground my-2 border-b pb-1 pl-1 text-xs first-of-type:mt-0 last-of-type:mb-0 last-of-type:border-none last-of-type:pb-0"
+      class="log-item"
+      :class="{
+        log: output.logLevel === 'log',
+        error: output.logLevel === 'error',
+        warn: output.logLevel === 'warn',
+        info: output.logLevel === 'info',
+        debug: output.logLevel === 'debug',
+      }"
     >
       {{ output.data }}
     </li>
@@ -20,6 +27,8 @@ const outputs = defineModel<OutputPreviewData[]>();
 </script>
 
 <style scoped>
+@reference '@/assets/styles.css';
+
 .appear-enter-active,
 .appear-leave-active {
   transition: height 0.5s;
@@ -28,5 +37,13 @@ const outputs = defineModel<OutputPreviewData[]>();
 .appear-enter-from,
 .appear-leave-to {
   height: 0;
+}
+
+.log-item {
+  @apply my-2 border-b pb-1 pl-2 text-xs first-of-type:mt-0 first-of-type:pt-2 last-of-type:mb-0 last-of-type:border-none last-of-type:pb-2;
+}
+
+.log {
+  @apply bg-background;
 }
 </style>
