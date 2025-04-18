7e7b549463288bca299dca6b02404bda03d2da02
Francisco <github.backstage538@passmail.net>
2025-04-18 13:42:25 +0200

    Fix focus

--- src/modules/settings/pages/settings-page.vue
+++ src/modules/settings/pages/settings-page.vue
@@ -9,7 +9,7 @@
           :validate-on-blur="!isFieldDirty"
         >
           <FormItem
-            class="border-muted-foreground border-l-2 py-0.5 pl-1 text-sm"
+            class="focus-within:border-muted-foreground border-l-2 py-0.5 pl-1 text-sm"
           >
             <div class="flex gap-1">
               <span class="font-semibold">Editor:</span>
