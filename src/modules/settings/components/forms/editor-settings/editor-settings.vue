6668c57211bb3e5747ebf17ac215981628de2285
Francisco <github.backstage538@passmail.net>
2025-04-19 21:02:51 +0200

    Add id

--- src/modules/settings/components/forms/editor-settings/editor-settings.vue
+++ src/modules/settings/components/forms/editor-settings/editor-settings.vue
@@ -31,7 +31,7 @@
         section-name="Editor"
         description="The number of spaces a tab is equal to."
       />
-      <h5>Formatter</h5>
+      <h5 id="formatter-settings">Formatter</h5>
       <FormInput
         :is-field-dirty="isFieldDirty"
         name="tabSize"
