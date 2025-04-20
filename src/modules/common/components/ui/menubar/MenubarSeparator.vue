0228b2b2c0f51f8ef0780a8628e4dd48fe48aa52
Francisco <github.backstage538@passmail.net>
2025-04-20 13:30:59 +0200

    Add menu component

---
+++ src/modules/common/components/ui/menubar/MenubarSeparator.vue
@@ -0,0 +1,30 @@
+<template>
+  <MenubarSeparator
+    data-slot="menubar-separator"
+    :class="cn('bg-border -mx-1 my-1 h-px', props.class)"
+    v-bind="forwardedProps"
+  />
+</template>
+
+<script setup lang="ts">
+import { cn } from '@/modules/common/helpers/utils';
+import {
+  MenubarSeparator,
+  type MenubarSeparatorProps,
+  useForwardProps,
+} from 'reka-ui';
+import { computed, type HTMLAttributes } from 'vue';
+
+const props = defineProps<
+  MenubarSeparatorProps & { class?: HTMLAttributes['class'] }
+>();
+
+const delegatedProps = computed(() => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { class: _, ...delegated } = props;
+
+  return delegated;
+});
+
+const forwardedProps = useForwardProps(delegatedProps);
+</script>
---
+++ src/modules/common/components/ui/menubar/MenubarTrigger.vue
@@ -0,0 +1,37 @@
+<template>
+  <MenubarTrigger
+    data-slot="menubar-trigger"
+    v-bind="forwardedProps"
+    :class="
+      cn(
+        'focus:bg-accent focus:text-accent-foreground data-[state=open]:bg-accent data-[state=open]:text-accent-foreground flex items-center rounded-sm px-2 py-1 text-sm font-medium outline-hidden select-none',
+        props.class,
+      )
+    "
+  >
+    <slot />
+  </MenubarTrigger>
+</template>
+
+<script setup lang="ts">
+import { cn } from '@/modules/common/helpers/utils';
+import {
+  MenubarTrigger,
+  type MenubarTriggerProps,
+  useForwardProps,
+} from 'reka-ui';
+import { computed, type HTMLAttributes } from 'vue';
+
+const props = defineProps<
+  MenubarTriggerProps & { class?: HTMLAttributes['class'] }
+>();
+
+const delegatedProps = computed(() => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { class: _, ...delegated } = props;
+
+  return delegated;
+});
+
+const forwardedProps = useForwardProps(delegatedProps);
+</script>
