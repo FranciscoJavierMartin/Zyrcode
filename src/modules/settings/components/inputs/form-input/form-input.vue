33629b39afbecec16efbfa68edccef1f969f45bc
Francisco <github.backstage538@passmail.net>
2025-04-21 20:27:22 +0200

    Move inputs

---
+++ src/modules/settings/components/inputs/form-input/form-input.vue
@@ -0,0 +1,53 @@
+<template>
+  <FormField
+    v-slot="{ componentField }"
+    :name
+    :validate-on-blur="!isFieldDirty"
+  >
+    <div class="settings-form-item-wrapper">
+      <FormItem class="settings-form-item">
+        <div class="flex gap-1">
+          <span class="font-semibold">{{ sectionName }}:</span>
+          <FormLabel class="font-bold">{{ label }}</FormLabel>
+        </div>
+        <FormDescription class="text-xs font-normal">
+          {{ description }}
+        </FormDescription>
+        <FormControl>
+          <Input
+            :id="name"
+            :type
+            :placeholder
+            class="h-7 w-max text-xs"
+            v-bind="componentField"
+          />
+        </FormControl>
+        <FormMessage />
+      </FormItem>
+    </div>
+  </FormField>
+</template>
+
+<!-- Add generic type -->
+<script setup lang="ts">
+import {
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormDescription,
+  FormMessage,
+} from '@/modules/common/components/ui/form';
+import { Input } from '@/modules/common/components/ui/input';
+import type { FormEditorFields } from '@/modules/settings/interfaces/form';
+
+defineProps<{
+  name: string;
+  label: string;
+  type: string;
+  description: string;
+  sectionName: string;
+  placeholder: string;
+  isFieldDirty: <TPath extends FormEditorFields>(path: TPath) => boolean;
+}>();
+</script>
---
+++ src/modules/settings/components/inputs/form-toggle/form-toggle.vue
@@ -0,0 +1,55 @@
+<template>
+  <FormField v-slot="{ value, handleChange }" type="checkbox" :name>
+    <div class="settings-form-item-wrapper">
+      <FormItem class="settings-form-item">
+        <div class="flex gap-x-1">
+          <span class="font-semibold">{{ sectionName }}:</span>
+          <span class="font-bold">{{ label }}</span>
+        </div>
+        <div class="flex items-center">
+          <FormControl>
+            <Checkbox :model-value="value" @update:model-value="handleChange" />
+            <FormLabel class="text-muted-foreground pl-2 text-xs font-normal">
+              {{ description }}
+            </FormLabel>
+          </FormControl>
+          <FormMessage />
+        </div>
+      </FormItem>
+    </div>
+  </FormField>
+</template>
+
+<!-- TODO: Add better generic types for the form fields  -->
+<script
+  setup
+  lang="ts"
+  generic="
+    T extends
+      | (<TPath extends FormEditorFields>(path: TPath) => boolean)
+      | (<TPath extends FormAIFields>(path: TPath) => boolean)
+      | (<TPath extends FormCommonFields>(path: TPath) => boolean)
+  "
+>
+import Checkbox from '@/modules/common/components/ui/checkbox/Checkbox.vue';
+import {
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormMessage,
+} from '@/modules/common/components/ui/form';
+import type {
+  FormEditorFields,
+  FormAIFields,
+  FormCommonFields,
+} from '@/modules/settings/interfaces/form';
+
+defineProps<{
+  name: string;
+  label: string;
+  description: string;
+  sectionName: string;
+  isFieldDirty: T;
+}>();
+</script>
