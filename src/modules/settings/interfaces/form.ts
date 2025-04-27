1117d43dc399f08831c6ec850296b8a5ff05338a
Francisco <github.backstage538@passmail.net>
2025-04-27 14:19:46 +0200

    Parse schema at upload

--- src/modules/settings/interfaces/form.ts
+++ src/modules/settings/interfaces/form.ts
@@ -4,6 +4,6 @@ import type {
   editorSchema,
 } from '@/modules/settings/helpers/schemas';
 
-export type FormEditorFields = keyof typeof editorSchema.shape;
-export type FormAIFields = keyof typeof aiSchema.shape;
-export type FormCommonFields = keyof typeof commonSchema.shape;
+export type FormEditorFields = keyof typeof editorSchema.entries;
+export type FormAIFields = keyof typeof aiSchema.entries;
+export type FormCommonFields = keyof typeof commonSchema.entries;
