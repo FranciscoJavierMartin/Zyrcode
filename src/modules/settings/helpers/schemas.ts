b185a5ade88bc80dcbeaf391324dedaa2916d2ae
Francisco <github.backstage538@passmail.net>
2025-04-28 23:09:55 +0200

    Formatter schema

--- src/modules/settings/helpers/schemas.ts
+++ src/modules/settings/helpers/schemas.ts
@@ -9,12 +9,15 @@ export const editorSchema = v.object({
   tabSize: v.pipe(v.number(), v.integer(), v.minValue(2), v.maxValue(30)),
   showLineNumbers: v.boolean(),
   language: v.pipe(v.string(), v.minLength(1)),
+  ruler: v.pipe(v.number(), v.integer(), v.minValue(70), v.maxValue(256)),
+});
+
+export const formatterSchema = v.object({
   semi: v.boolean(),
   singleQuote: v.boolean(),
   jsxSingleQuote: v.boolean(),
   useTabs: v.boolean(),
   printWidth: v.pipe(v.number(), v.integer(), v.minValue(70)),
-  ruler: v.pipe(v.number(), v.integer(), v.minValue(70), v.maxValue(256)),
   trailingComma: v.union([
     v.literal('none'),
     v.literal('es5'),
--- src/modules/settings/interfaces/form.ts
+++ src/modules/settings/interfaces/form.ts
@@ -3,12 +3,15 @@ import type {
   aiSchema,
   commonSchema,
   editorSchema,
+  formatterSchema,
 } from '@/modules/settings/helpers/schemas';
 
 export type FormCommonSettings = InferOutput<typeof commonSchema>;
 export type FormEditorSettings = InferOutput<typeof editorSchema>;
+export type FormFormatterSettings = InferOutput<typeof formatterSchema>;
 export type FormAISettings = InferOutput<typeof aiSchema>;
 
 export type FormCommonFields = keyof typeof commonSchema.entries;
 export type FormEditorFields = keyof typeof editorSchema.entries;
+export type FormFormatterFields = keyof typeof formatterSchema.entries;
 export type FormAIFields = keyof typeof aiSchema.entries;
